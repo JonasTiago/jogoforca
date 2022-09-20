18f26d9dcdaeb67b2217b1babd16e0a1b4e07314
Jonas Tiago <jonastiago95@gmail.com>
2022-09-20 17:02:58 -0300

    layout: jogo iniciado adicionado

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,71 +1,77 @@
 import palavras from "./palavras";
-
-function Forca(props) {
-
-    return (
-        <div class="forca">
-            <img src={props.imagem} />
-        </div>
-    )
-}
-
-function BtInicio() {
-    return (
-        <>
-            <button>Escolher Palavra</button>
-        </>
-    )
-}
-
-function Palavra(props) {
-    return (
-        <>
-            <span class="palavraEscolhida">_ _ _ _ _ _ _ _ </span>
-        </>
-    )
-}
-
-function Letras(props) {
-    console.log(props)
-    const {letras, estado} = props
-    
-
-    return (
-        <div class="alfabeto">
-            {letras.map(letra => <button class={estado}>{letra}</button>)}
-        </div>
-    )
-
-}
+import { useState } from "react";
 
 function Chute() {
+
     return (
         <div class="chute">
             <p>Já sei a palavra!</p>
             <input type="text" />
-            <button>chutar</button>
+            <button >chutar</button>
         </div>
     )
 }
 
+function Letras(props) {
+
+    const letras = props.letra
+
+    return (
+
+        <div class="alfabeto">
+            {letras.map(letra => <button >{letra}</button>)}
+        </div>
+
+    )
+}
+
+function Jogo(props) {
+
+    const { todasPalavras, letras, imagem } = props
+    const [erro, setErro] = useState(0)
+    const [inicio, setInicio] = useState('disabled')
+
+
+    function errou() {
+        return setErro(erro + 1)
+    }
+
+    const [palavra, setPalavra] = useState('');
+
+    function iniciar() {
+        console.log(palavra)
+        todasPalavras.sort(()=>  Math.random() - 0.5)
+        console.log(todasPalavras[0])
+        setPalavra(
+            todasPalavras[0]
+        )
+        
+    }
+
+    return (
+        <div class="jogo">
+            <div class="section1">
+                <img class="forca" src={imagem[erro]} />
+                <div class="inicio">
+                    <button onClick={iniciar}>Escolher Palavra</button>
+                    <span class="palavraEscolhida">{palavra.split('').map((letra) => <p>_ </p>)}</span>
+                </div>
+            </div>
+            <div class="section2">
+                <Letras letra={letras} />
+                <Chute />
+            </div>
+        </div>
+    )
+
+}
+
 export default function App() {
-    const todasPalavras = palavras;
+    const imgForca = ['assets/img/forca0.png', 'assets/img/forca1.png', 'assets/img/forca2.png', 'assets/img/forca3.png', 'assets/img/forca4.png', 'assets/img/forca5.png', 'assets/img/forca6.png']
+
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
     return (
-        <main class="jogo">
-            <section class='section1'>
-                <Forca imagem="assets/img/forca0.png" />
-                <div class="inicio">
-                    <BtInicio />
-                    <Palavra palavras={todasPalavras} />
-                </div>
-            </section>
-
-            <section class="section2">
-                <Letras letras={alfabeto} estado={'desativado'} />
-                <Chute />
-            </section>
-        </main>
+        <Jogo todasPalavras={palavras} letras={alfabeto} imagem={imgForca} />
     )
 }
